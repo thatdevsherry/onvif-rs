64754c796dcc265cf728cbe90e78d9fcc0236092
Shehriyar Qureshi <thatdevsherry@gmail.com>
2022-10-15 21:18:00 +0500

    feat(discovery): use constants for some strings

--- lib/src/operations/discovery.rs
+++ lib/src/operations/discovery.rs
@@ -1,17 +1,27 @@
+use serde::{Deserialize, Serialize};
 use tokio::net::UdpSocket;
 
 use anyhow::Result;
+const WS_DISCOVERY_IP_MULTICAST_ADDRESS: &'static str = "239.255.255.250";
+const WS_DISCOVERY_PORT: &'static str = "3702";
+const UDP_SOCKET_ADDR: &'static str = "0.0.0.0:0"; // let OS choose port
 
 use crate::{
     soap::{Envelope, Soap},
-    wsdl::probe::{Probe, ProbeMatches},
+    wsdl::probe::{Probe, ProbeMatch, ProbeMatches},
 };
+
+#[derive(Debug, Serialize, Deserialize, PartialEq)]
+pub struct DiscoveryParsed {
+    name: Option<String>,
+    country: Option<String>,
+    profiles: Vec<String>,
+}
 
 pub async fn discover_onvif_devices() -> Result<()> {
     info!("Preparing to discover devices");
-    // Create a udp socket on any port provided by OS
     debug!("Binding socket");
-    let sock = UdpSocket::bind("0.0.0.0:0").await?;
+    let sock = UdpSocket::bind(UDP_SOCKET_ADDR).await?;
     debug!("Successfully binded: {}", sock.local_addr()?);
 
     let payload = Probe::default();
@@ -22,15 +32,19 @@ pub async fn discover_onvif_devices() -> Result<()> {
     let request_as_bytes = serialized.as_bytes();
 
     // Send the Discovery XML to IP Multicast
-    let target = "239.255.255.250:3702";
-    debug!("Sending payload");
+    let target = format!(
+        "{}:{}",
+        WS_DISCOVERY_IP_MULTICAST_ADDRESS, WS_DISCOVERY_PORT
+    );
+    debug!("Sending payload to: {}", target);
     sock.send_to(&request_as_bytes, target).await?;
     debug!("Payload sent");
 
     let mut recv_buf: [u8; 1500] = [0; 1500];
     debug!("Listening for response");
     let n = sock.recv(&mut recv_buf).await?;
-    debug!("We received something: {}", n);
+    debug!("We received something of size: {}", n);
+    info!("Found a device");
     let response_string = String::from_utf8_lossy(&recv_buf).into_owned();
     debug!("Response: {}", response_string);
     let deserialize_response =
