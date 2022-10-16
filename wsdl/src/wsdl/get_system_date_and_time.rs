eee420fe3e69e3ac6f79209b6adc6af1bcd2b123
Shehriyar Qureshi <thatdevsherry@gmail.com>
2022-10-16 17:00:28 +0500

    chore(): some adjustments to deps

--- wsdl/src/wsdl/get_system_date_and_time.rs
+++ wsdl/src/wsdl/get_system_date_and_time.rs
@@ -19,31 +19,3 @@ impl OnvifOperation for GetSystemDateAndTimeResponse {
         "GetSystemDateAndTimeResponse"
     }
 }
-
-// #[cfg(test)]
-// mod tests {
-//     use crate::{
-//         create_soap_request,
-//         soap::{Envelope, Soap},
-//         wsdl::get_system_date_and_time::{GetSystemDateAndTime, GetSystemDateAndTimeResponse},
-//     };
-
-//     #[test]
-//     fn test_soap_request_creation_for_get_system_date_and_time() {
-//         let get_system_date_and_time = GetSystemDateAndTime {};
-//         let soap_request_string = create_soap_request(get_system_date_and_time).unwrap();
-//         assert_eq!(
-//             "<Envelope><Body><GetSystemDateAndTime/></Body></Envelope>",
-//             soap_request_string
-//         );
-//     }
-
-//     #[test]
-//     fn test_deserialize_get_system_date_and_time_response() {
-//         let input = "<Envelope><Body><GetSystemDateAndTimeResponse/></Body></Envelope>";
-//         let actual =
-//             quick_xml::de::from_str::<Envelope<GetSystemDateAndTimeResponse>>(input).unwrap();
-//         let expected = GetSystemDateAndTimeResponse {}.apply_soap();
-//         assert_eq!(expected, actual);
-//     }
-// }
